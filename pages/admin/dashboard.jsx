443f4a1b9a31b7644cc2496dd270c96ebc6bd6e1
Labhansh Satpute <labhansh25@outlook.in>
2022-11-24 10:23:54 +0530

    datatable integration

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Admin from "../../layouts/Admin";
 
+import DataTable from 'react-data-table-component';
+
 const Dashboard = () => {
+
+  const columns = [
+      {
+          name: 'Type',
+          selector: row => row.type,
+          sortable: true,
+          cell:(row) => <button onClick={() => alert('dsfsd')} >{row.type}</button>,
+      },
+      {
+          name: 'Login',
+          selector: row => row.login,
+          sortable: true,
+      },
+      {
+          name: 'Node Id',
+          selector: row => row.node_id,
+          sortable: true,
+      },
+  ];
+
+  const [users,setUsers] = useState(null);
+
+  useEffect(() => {
+    try {
+      fetch('https://api.github.com/users')
+      .then((response) => { 
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },[]);
+
   return (
     <React.Fragment>
       <div>
@@ -16,8 +55,8 @@ const Dashboard = () => {
               <p className="text-xs text-slate-500">All registred designers list</p>
             </div>
           </div>
-          <div className="p-3 bg-slate-100">
-
+          <div className="p-0 bg-slate-100 border-y">
+          { (users !== null) ? <DataTable columns={columns} data={users} pagination /> :null }
           </div>
         </figure>
         
